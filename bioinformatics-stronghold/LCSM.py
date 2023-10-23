db0178072f7cc73ff27cab2f0d8ec35673c2da7f
Hà Trọng Nghĩa <nghia.hust@gmail.com>
2023-10-23 17:40:35 +0700

    Upload

--- bioinformatics-stronghold/LCSM.py
+++ bioinformatics-stronghold/LCSM.py
@@ -13,31 +13,19 @@ for i in data:
         seqs[i[1::].strip()] = ""
     else:
         seqs[list(seqs.keys())[-1]] += i.strip()
-# Extract all possible sub sequences
+# Extract all sub sequences
 min_seq = min(list(seqs.values()))
 n = len(min_seq)
-sub_seqs = []
+subseqs = []
 for i in range(n,1,-1):
     for j in range(0,n-i+1):
-        sub_seqs.append(min_seq[j:i+j])
-# Find longest common subsequences by binary search
-l = 0
-r = len(sub_seqs)
-found = False
-def sub_in_all(sub, seqs):
-    for seq in seqs:
-        if sub not in seq:
-            return False
-    return True
-
-while found == False and l + 1 > mid :
-    for seq in seqs:
-        mid = (l + r) // 2
-        if sub_in_all(sub_seqs[mid], seqs) == True:
-            r = mid
-        else:
-            l = mid            
-            
+        subseqs.append(min_seq[j:j+i])
+# Fidn common substring
+def common_sub(seqs, sub):
+    return all(sub in seq for seq in seqs)
+# Find longest common sub-sequence
+def longest_common(seqs):
+    
     
 # Close files
 result.close()
